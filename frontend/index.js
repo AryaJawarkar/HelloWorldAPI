6cc70c45558c2996d31d7ffb3b53d64a014e9a71
Arya Jawarkar <aryajawarkar7@gmail.com>
2024-11-16 14:03:54 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,11 @@
 const Greet = document.getElementById("formlang");
 const disp = document.getElementById("result-display");
+const url = 'https://helloworldapi-m2oj.onrender.com';
 Greet.addEventListener("submit",async (e) =>{
     e.preventDefault();
     try {
         const lang = e.target["language"].value;
-        const res = await fetch(`http://localhost:5000/hello?language=${lang}`,{
+        const res = await fetch(url+`/hello?language=${lang}`,{
             method:"GET"
         });
         const result = await res.json();
